5e9bfe545d063c141a22968dbab0c7fbb17baa39
jiwei <jiweimic@hotmail.com>
2021-06-12 22:15:15 +0800

    Hello Spring Mvc

---
+++ springmvc-4-controller/src/main/java/com/jiwei/controller/ControllerTest1.java
@@ -0,0 +1,16 @@
+package com.jiwei.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ControllerTest1 implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "ControllerTest1");
+        modelAndView.setViewName("test1");
+        return modelAndView;
+    }
+}
